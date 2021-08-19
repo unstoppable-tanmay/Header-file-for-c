d251218b5542b5cdf5910e5277e53efe1a45ca20
unstoppableTANMAY <85018447+unstoppableTANMAY@users.noreply.github.com>
2021-08-19 22:03:41 +0530

    Add files via upload
    ok

---
+++ demo.c
@@ -0,0 +1,13 @@
+#include "let.h"
+
+ void main()
+ {
+ 	char c[50];
+ 	printf("Enter something..");
+ 	gets(c);
+	char font = "|";
+ 	print(c,font);
+ 	printf("Enter something and then enter to exit..");
+ 	int i;
+ 	scanf("%d",&i);
+ }
